bf5c5df1af64f5cd137e6400028565e5fd2f5382
gram <git@orsinium.dev>
2024-11-30 12:01:42 +0100

    draft embedded device skeleton

--- src/embedded.rs
+++ src/embedded.rs
@@ -1,23 +1,155 @@
-pub struct DeviceImpl {
-    start: std::time::Instant,
-    gilrs: Gilrs,
-    gamepad_id: Option<GamepadId>,
-}
+use crate::shared::{Device, Network, Serial};
 
-pub fn get_device() -> &'static mut DeviceImpl {
-    todo!()
-}
+pub struct DeviceImpl {}
 
 impl Device for DeviceImpl {
-    fn now(&self) -> Time {
+    type Read = File;
+    type Write = File;
+    type Network = NetworkImpl;
+    type Serial = SerialImpl;
+
+    fn now(&self) -> crate::Instant {
         todo!()
     }
 
-    fn delay(&self, d: Delay) {
+    fn delay(&self, d: crate::Duration) {
         todo!()
     }
 
-    fn read_input(&mut self) -> Option<InputState> {
+    fn read_input(&mut self) -> Option<crate::InputState> {
+        todo!()
+    }
+
+    fn log_debug<D: core::fmt::Display>(&self, src: &str, msg: D) {
+        todo!()
+    }
+
+    fn log_error<D: core::fmt::Display>(&self, src: &str, msg: D) {
+        todo!()
+    }
+
+    fn open_file(&self, path: &[&str]) -> Option<Self::Read> {
+        todo!()
+    }
+
+    fn create_file(&self, path: &[&str]) -> Option<Self::Write> {
+        todo!()
+    }
+
+    fn append_file(&self, path: &[&str]) -> Option<Self::Write> {
+        todo!()
+    }
+
+    fn get_file_size(&self, path: &[&str]) -> Option<u32> {
+        todo!()
+    }
+
+    fn make_dir(&self, path: &[&str]) -> bool {
+        todo!()
+    }
+
+    fn remove_file(&self, path: &[&str]) -> bool {
+        todo!()
+    }
+
+    fn iter_dir<F>(&self, path: &[&str], f: F) -> bool
+    where
+        F: FnMut(crate::EntryKind, &[u8]),
+    {
+        todo!()
+    }
+
+    fn network(&self) -> Self::Network {
+        todo!()
+    }
+
+    fn serial(&self) -> Self::Serial {
+        todo!()
+    }
+
+    fn has_headphones(&mut self) -> bool {
+        todo!()
+    }
+
+    fn get_audio_buffer(&mut self) -> &mut [i16] {
+        todo!()
+    }
+}
+
+pub struct File {}
+
+impl embedded_io::ErrorType for File {
+    type Error = embedded_io::ErrorKind;
+}
+
+impl embedded_io::Write for File {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+        todo!()
+    }
+
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        todo!()
+    }
+}
+
+impl embedded_io::Read for File {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+        todo!()
+    }
+}
+
+impl wasmi::Read for File {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, wasmi::errors::ReadError> {
+        todo!()
+    }
+}
+
+pub struct NetworkImpl {}
+
+impl Network for NetworkImpl {
+    type Addr = ();
+
+    fn start(&mut self) -> crate::NetworkResult<()> {
+        todo!()
+    }
+
+    fn stop(&mut self) -> crate::NetworkResult<()> {
+        todo!()
+    }
+
+    fn local_addr(&self) -> Self::Addr {
+        todo!()
+    }
+
+    fn advertise(&mut self) -> crate::NetworkResult<()> {
+        todo!()
+    }
+
+    fn recv(&mut self) -> crate::NetworkResult<Option<(Self::Addr, heapless::Vec<u8, 64>)>> {
+        todo!()
+    }
+
+    fn send(&mut self, addr: Self::Addr, data: &[u8]) -> crate::NetworkResult<()> {
+        todo!()
+    }
+}
+
+pub struct SerialImpl {}
+
+impl Serial for SerialImpl {
+    fn start(&mut self) -> crate::NetworkResult<()> {
+        todo!()
+    }
+
+    fn stop(&mut self) -> crate::NetworkResult<()> {
+        todo!()
+    }
+
+    fn recv(&mut self) -> crate::NetworkResult<Option<heapless::Vec<u8, 64>>> {
+        todo!()
+    }
+
+    fn send(&mut self, data: &[u8]) -> crate::NetworkResult<()> {
         todo!()
     }
 }
